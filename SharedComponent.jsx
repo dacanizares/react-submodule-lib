6e6328cd90379446470847fbf35546cc8813e00b
dacanizares <dacanizares@outlook.com>
2023-09-13 22:59:24 -0500

    Added lib files

---
+++ SharedComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './SharedComponent.module.css';
+
+const SharedComponent = () => (
+    <b className={styles.shared_css}>
+        This is a shared component
+    </b>
+);
+
+export default SharedComponent;
